7325b0bc6cd92fac97ccba2c6fedeef03f7fe6ac
Matheus Bonnet <matheusbonnet2019@gmail.com>
2021-09-01 12:34:51 -0300

    Proeto criado

---
+++ src/main/java/com/matheus/aep/AepDoacaoApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.aep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AepDoacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AepDoacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/aep/AepDoacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.aep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AepDoacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
